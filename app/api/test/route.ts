0795dd8985479ce73ce8fd0ba9ea3efe82051b4d
morronepablo <morronepablo@gmail.com>
2025-02-15 12:17:03 -0300

    add workflow 5

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,6 +1,16 @@
+// import { dbConnect } from "@/lib/dbConnect";
+
+// export async function GET() {
+//   await dbConnect();
+//   return Response.json({ message: "message" }, { status: 200 });
+// }
+
+import { NextResponse } from "next/server";
 import { dbConnect } from "@/lib/dbConnect";
+
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   await dbConnect();
-  return Response.json({ message: "message" }, { status: 200 });
+  return NextResponse.json({ message: "Connected successfully" });
 }
--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,13 +1,30 @@
+// import mongoose from "mongoose";
+
+// export async function dbConnect() {
+//   //check if moongoose is already connected
+//   if (mongoose.connection.readyState >= 1) {
+//     console.log("Already connected");
+
+//     return;
+//   }
+
+//   await mongoose.connect(process.env.MONGO_URL as string);
+//   console.log("Connected to MongoDB");
+// }
+
 import mongoose from "mongoose";
 
 export async function dbConnect() {
-  //check if moongoose is already connected
   if (mongoose.connection.readyState >= 1) {
     console.log("Already connected");
-
     return;
   }
 
-  await mongoose.connect(process.env.MONGO_URL as string);
+  const mongoUri = process.env.MONGO_URL as string;
+  if (!mongoUri) {
+    throw new Error("MONGO_URL is not defined");
+  }
+
+  await mongoose.connect(mongoUri);
   console.log("Connected to MongoDB");
 }
